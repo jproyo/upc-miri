349e869739fbee7af806691a527683a354531a54
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-03-07 22:11:22 +0100

    Improving

--- ads/homeworks/homework1/random-bst/src/RandomBST.hs
+++ ads/homeworks/homework1/random-bst/src/RandomBST.hs
@@ -1,15 +1,22 @@
+{-
+All this source code was taken and modified from here https://wiki.haskell.org/The_Monad.Reader/Issue4/On_Treaps_And_Randomization
+-}
 module RandomBST
   ( insert
   , delete
   , empty
   , height
   , fromList
+  , RTreap
   )
 where
+
+------------------------------------------------------------------------------------------------------------------------
 
 import           System.Random
 import           Prelude                 hiding ( null )
 
+------------------------------------------------------------------------------------------------------------------------
 
 data Treap k p = Empty | Node (Treap k p) k p (Treap k p)
  deriving (Show, Read)
@@ -17,13 +24,15 @@ data Treap k p = Empty | Node (Treap k p) k p (Treap k p)
 newtype RTreap g k p = RT (g, Treap k p)
  deriving (Show, Read)
 
-treapEmpty :: Treap k p
-treapEmpty = Empty
+empty :: RandomGen g => g -> RTreap g k p
+empty g = RT (g, tEmpty)
 
-treapHeight :: Treap k p -> Int
-treapHeight Empty = 0
-treapHeight (Node left _ _ right) =
-  1 + max (treapHeight left) (treapHeight right)
+tEmpty :: Treap k p
+tEmpty = Empty
+
+tHeight :: Treap k p -> Int
+tHeight Empty                 = 0
+tHeight (Node left _ _ right) = 1 + max (tHeight left) (tHeight right)
 
 rotateLeft :: Treap k p -> Treap k p
 rotateLeft (Node a k p (Node b1 k' p' b2)) = Node (Node a k p b1) k' p' b2
@@ -33,24 +42,24 @@ rotateRight :: Treap k p -> Treap k p
 rotateRight (Node (Node a1 k' p' a2) k p b) = Node a1 k' p' (Node a2 k p b)
 rotateRight _ = error "Wrong rotation (rotateRight)"
 
-treapInsert :: (Ord k, Ord p) => k -> p -> Treap k p -> Treap k p
-treapInsert k p Empty                   = Node Empty k p Empty
-treapInsert k p (Node left k' p' right) = case compare k k' of
-  EQ -> Node left k' p' right -- Node is already there, ignore
-  LT -> case Node (treapInsert k p left) k' p' right of
-    t@(Node (Node _ _ p'' _) _ p''' _) ->
-      if p''' > p'' then rotateRight t else t
-    t -> t
-  GT -> case Node left k' p' (treapInsert k p right) of
-    t@(Node _ _ p''' (Node _ _ p'' _)) ->
-      if p''' > p'' then rotateLeft t else t
-    t -> t
+tInsert :: (Ord k, Ord p) => k -> p -> Treap k p -> Treap k p
+tInsert k p Empty                     = Node Empty k p Empty
+tInsert k p t@(Node left k' p' right) = case compare k k' of
+  EQ -> t
+  LT -> case Node (tInsert k p left) k' p' right of
+    t'@(Node (Node _ _ p'' _) _ p''' _) ->
+      if p''' > p'' then rotateRight t' else t'
+    _ -> t
+  GT -> case Node left k' p' (tInsert k p right) of
+    t'@(Node _ _ p''' (Node _ _ p'' _)) ->
+      if p''' > p'' then rotateLeft t' else t'
+    _ -> t
 
 
-treapDelete :: (Ord k, Ord p) => k -> Treap k p -> Treap k p
-treapDelete = recDelete
+tDelete :: (Ord k, Ord p) => k -> Treap k p -> Treap k p
+tDelete = recDelete
  where
-  recDelete _ Empty = error "Key does not exist in tree (treapDelete)"
+  recDelete _ Empty = error "Key does not exist in tree (tDelete)"
   recDelete k' t@(Node left k'' p right) = case compare k' k'' of
     LT -> Node (recDelete k' left) k'' p right
     GT -> Node left k'' p (recDelete k' right)
@@ -71,8 +80,6 @@ treapDelete = recDelete
       let Node left' k p right' = rotateLeft t
       in  Node (rootDelete left') k p right'
 
-empty :: RandomGen g => g -> RTreap g k p
-empty g = RT (g, treapEmpty)
 
 insert
   :: (RandomGen g, Ord k, Ord p, Num p, Random p)
@@ -80,14 +87,14 @@ insert
   -> RTreap g k p
   -> RTreap g k p
 insert k (RT (g, tr)) =
-  let (p, g') = randomR (-2000000000, 2000000000) g
-  in  RT (g', treapInsert k p tr)
+  let (p, g') = randomR (-2000000000, 2000000000) g in RT (g', tInsert k p tr)
 
 delete :: (RandomGen g, Ord k, Ord p) => k -> RTreap g k p -> RTreap g k p
-delete k (RT (g, tr)) = RT (g, treapDelete k tr)
+delete k (RT (g, tr)) = RT (g, tDelete k tr)
 
 height :: RTreap g k p -> Int
-height (RT (_, treap)) = treapHeight treap
+height (RT (_, treap)) = tHeight treap
+
 
 fromList
   :: (Ord a, Ord p, Num p, Random p, RandomGen g) => [a] -> g -> RTreap g a p
--- ads/homeworks/homework1/random-bst/test/Spec.hs
+++ ads/homeworks/homework1/random-bst/test/Spec.hs
@@ -1,20 +1,56 @@
-import Test.Framework (defaultMain, testGroup)
-import Test.Framework.Providers.QuickCheck2 (testProperty)
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving  #-}
 
-import Test.QuickCheck
+import           Test.Framework                 ( defaultMain
+                                                , testGroup
+                                                )
+import           Test.Framework.Providers.QuickCheck2
+                                                ( testProperty )
+import           Data.List
+import           RandomBST
+import           System.Random
+
+import           Test.QuickCheck
+
+
+newtype ListArrayTest = ListArrayTest { unList :: [Int] }
+    deriving newtype Show
+
+instance Arbitrary ListArrayTest where
+  arbitrary =
+    ListArrayTest
+      <$>        (listOf $ choose (100, 2000) :: Gen [Int])
+      `suchThat` isUnique
+    where isUnique x = nub x == x
 
 main :: IO ()
-main = defaultMain tests
+main = do
+  gen <- newStdGen
+  defaultMain $ tests gen
 
-tests = [
-        testGroup "Sorting Group 1" [
-                testProperty "prop1" prop1,
-                testProperty "prop2" prop2
-           ]
+tests gen =
+  [ testGroup
+      "Insertion RandomBST with height in log_n time"
+      [ testProperty "Insertion with random lists from 10 to 2000 elems cases"
+                     (withMaxSuccess 1000 (insertionRandom gen))
       ]
+  ]
 
-prop1 b = b == False
-  where types = (b :: Bool)
+generateLists :: Gen [Int]
+generateLists = unList <$> (arbitrary :: Gen ListArrayTest) `suchThat` \l -> (not $ Data.List.null (unList l)) && (length $ (unList l)) > 20
 
-prop2 i = i == 42
-  where types = (i :: Int)
+insertionRandom :: StdGen -> Property
+insertionRandom gen =
+  forAll generateLists $ \xs ->
+  let
+    lengthList    = length xs
+    expected =
+      (logBase ((fromIntegral 2) :: Float) ((fromIntegral lengthList) :: Float))
+        * 3
+  in
+    fromIntegral (height $ (fromList xs gen :: RTreap StdGen Int Int))
+      <= expected
+  where types = gen::StdGen
+

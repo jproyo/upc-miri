557d850ab392ec5847d1b2ccc8a610be10b0de46
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-03-09 09:36:25 +0100

    Refactor

--- ads/homeworks/homework1/random-bst/src/Experiments.hs
+++ ads/homeworks/homework1/random-bst/src/Experiments.hs
@@ -1,42 +1,49 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Experiments where
 
 
 import           Test.QuickCheck
 import           Data.List
 import           System.Random
-import           RandomBST as RBST
+import           RandomBST                     as RBST
+import           Data.Default
+import           GHC.Generics
 
 experiment :: StdGen -> Int -> IO String
 experiment gen n = do
   number <- (generate $ choose (0, 10000000)) :: IO Int
-  let list = [number..number+n-1]
-  let lengthList = length list
-  let action     = fromList list gen :: RTreap StdGen Int Int
-  let actionDel  = RBST.delete (list !! ((lengthList `div` 2)-1)) action
-  let heightA    = height action
-  let heightD    = height actionDel
-  let expectedHeight = logBase (2 :: Float) (fromIntegral lengthList)
-  let varHeight = abs (fromIntegral heightA - expectedHeight)
-  let leafD    = leafDepth action
-  let avgLeafD = average $ map fromIntegral leafD :: Double
-  let varLeafD = variance $ map fromIntegral leafD :: Double
-  let varHeightDel = abs (fromIntegral heightD - expectedHeight)
-  let leafDDel    = leafDepth actionDel
-  let avgLeafDDel = average $ map fromIntegral leafDDel :: Double
-  let varLeafDDel = variance $ map fromIntegral leafDDel :: Double
-  let insertExpOnly = intercalate "," [show lengthList, show heightA, show expectedHeight, show varHeight, show avgLeafD, show varLeafD] <> "\n"
-  let insertDelExp = intercalate "," [show lengthList, show heightD, show expectedHeight, show varHeightDel, show avgLeafDDel, show varLeafDDel] <> "\n"
-  return $ insertExpOnly <> insertDelExp
+  let list      = [number .. number + n - 1]
+  let action    = fromList list gen :: RTreap StdGen Int Int
+  let actionDel = RBST.delete (list !! ((n `div` 2) - 1)) action
+  return $ foldMap (show . experiment') [action, actionDel]
 
-generateList :: Int -> Gen [Int]
-generateList n =
-  (vectorOf n (choose (0, 10000000)) :: Gen [Int]) `suchThat` isUnique
-  where isUnique x = nub x == x
+data Meassure = Meassure
+  { sizeList :: Int
+  , empHeight  :: Int
+  , expHeight :: Double
+  , avgLeaf :: Double
+  } deriving (Generic, Default)
+
+instance Show Meassure where
+  show Meassure {..} = intercalate
+    ","
+    [show sizeList, show empHeight, show expHeight, show avgLeaf] <> "\n"
+
+experiment' :: RandomGen g => RTreap g Int Int -> Meassure
+experiment' rbst =
+  let listL = nodes rbst
+  in  Meassure { sizeList  = listL
+               , empHeight = height rbst
+               , expHeight = expectedHeight listL
+               , avgLeaf   = average $ map fromIntegral $ leafDepth rbst
+               }
+
+expectedHeight :: Int -> Double
+expectedHeight = logBase 2 . fromIntegral
 
 average :: Fractional a => [a] -> a
 average xs = sum xs / fromIntegral (length xs)
-
-variance :: Fractional a => [a] -> a
-variance xs = sum (map ((^ (2 :: Int)) . subtract (average xs)) xs)
-  / fromIntegral (length xs - 1)
 

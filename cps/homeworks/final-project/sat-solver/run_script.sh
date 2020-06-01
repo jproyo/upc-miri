e41f5594f40a27c2b06ffe4060ff2a6f2f95f86a
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-06-01 14:25:29 +0200

    First round

--- cps/homeworks/final-project/sat-solver/run_script.sh
+++ cps/homeworks/final-project/sat-solver/run_script.sh
@@ -4,20 +4,18 @@ TIME_LIMIT=60s
 INST_DIR=`pwd`/instances
 OUTPUT_DIR=`pwd`/out
 INST_EXT=inp
-BOX_EXEC=`pwd`/bin/boxw
-CHECK_EXEC=`pwd`/bin/checker
 
 echo "COMPILING"
-make clean && make
+stack build
 
-echo "----------------- STARTING !!!! BOX WRAPPING LP PROGRAM ---------------------"
+echo "----------------- STARTING !!!! BOX WRAPPING SAT PROGRAM ---------------------"
 
 for ifile in $INST_DIR/*.$INST_EXT; do
     output=$(basename $ifile .$INST_EXT).out
-    timeout $TIME_LIMIT $BOX_EXEC < $ifile > $OUTPUT_DIR/$output
+    timeout $TIME_LIMIT stack exec sat < $ifile > $OUTPUT_DIR/$output
     if [ $? != 0 ]; then
     	echo "Instance $ifile has TIMED OUT"
     fi
 done
-echo "----------------- FINISHED !!!! BOX WRAPPING LP PROGRAM ---------------------"
+echo "----------------- FINISHED !!!! BOX WRAPPING SAT PROGRAM ---------------------"
 

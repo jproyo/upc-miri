b91942146dd44163626c0b3223011a17cd65403a
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-11-20 19:25:18 +0100

    Fixing script

--- csn/homeworks/homework_5/Script.R
+++ csn/homeworks/homework_5/Script.R
@@ -26,8 +26,8 @@ calculate_tpr <- function(community, g) {
 }
 
 # Lower the best f_c/n_c
-calculate_expansion <- function(f_c, g){
-  return(f_c/gsize(g))
+calculate_expansion <- function(f_c, g_size){
+  return(sum(f_c)/g_size)
 }
 
 calculate_modularity = modularity
@@ -37,37 +37,52 @@ calculate_modularity = modularity
 get_f_c_m_c <- function(community, g){
   # Take all members (vertices) of the community that we are calculating
   members <- membership(community)
+  n <- length(community)
   # Take all edges of the graph
   edges <- E(g)
   sum_m_c <- 0
   sum_f_c <- 0
+  out_degree <- rep(0, n)
+  in_degree <- rep(0, n)
+  
   for(edge in edges) {
     e <- ends(g, edge)
-    if(members[e[1]] == members[e[2]]){
-      sum_m_c <- sum_m_c + 2
+    x <- e[1]
+    y <- e[2]
+    if(members[x] == members[y]){
+      in_degree[members[x]] = in_degree[members[x]] + 1
+      in_degree[members[y]] = in_degree[members[y]] + 1
     }else{
-      sum_f_c <- sum_f_c + 2
+      out_degree[members[x]] = out_degree[members[x]] + 1
+      out_degree[members[y]] = out_degree[members[y]] + 1
     }
   }
-  return(c(sum_f_c, sum_m_c))
+  return(list(in_degree, out_degree))
 }
 
 # Lower the best: Fraction of total edge volume that points outside the cluster, \frac{f_c}{2m_c+f_c}
-calculate_conductance <- function(f_c, m_c) {
-  return(f_c/((2*m_c)+f_c))
+calculate_conductance <- function(f_c, m_c, comm_sizes, g_size) {
+  n <- length(comm_sizes)
+  s <- 0
+  for(i in 1:n) {
+    s = s + f_c[i]*comm_sizes[i]/(m_c[i] + f_c[i])
+  }
+  return(s/g_size)
 }
 
 execute_algorithm <- function(algorithm, graph){
   community <- get(algorithm)(graph)
   tuple_f_c_m_c <- get_f_c_m_c(community, graph)
-  f_c <- tuple_f_c_m_c[1]
-  m_c <- tuple_f_c_m_c[2]
+  m_c <- tuple_f_c_m_c[[1]]
+  f_c <- tuple_f_c_m_c[[2]]
+  comm_sizes <- unname(sizes(community))
+  g_size <- gsize(graph)
   tpr <- calculate_tpr(community, graph)
-  expansion <- calculate_expansion(f_c, graph)
-  conductance <- calculate_conductance(f_c, m_c)
+  expansion <- calculate_expansion(f_c, g_size)
+  conductance <- calculate_conductance(f_c, m_c, comm_sizes, g_size)
   modularity <- calculate_modularity(community, graph)
   print(paste("Algorithm: ", algorithm, " was calculated"))
-  return(c(tpr, expansion, conductance, modularity))
+  return(c(round(tpr, 4), round(expansion, 4), round(conductance, 4), round(modularity,4)))
 }
 
 # Print extracted data for each algorithm and each metric
@@ -121,15 +136,16 @@ main <- function(){
   # Starred Repositories in Github https://snap.stanford.edu/data/github_stargazers.html
   graph_git <- graph.data.frame(read.csv('git_target.csv', header=TRUE), directed = FALSE)
   
+  # Facebook combined https://snap.stanford.edu/data/ego-Facebook.html
   graph_facebook <- read.graph("facebook_combined.txt", format="edgelist", directed = FALSE)
   
-  run_graph("Facebook", graph_facebook, communities_algo_big)
-  
-  #run_graph("Zachary", graph_zachary, communities_algo_small)
+  run_graph("Zachary", graph_zachary, communities_algo_small)
   
   # Comment this if you want to run something quick
-  #run_graph("Github Starred Repos", graph_git, communities_algo_big)
-  
+  run_graph("Github Starred Repos", graph_git, communities_algo_big)
+
+  # Comment this if you want to run something quick
+  run_graph("Facebook", graph_facebook, communities_algo_big)
 
 }
 

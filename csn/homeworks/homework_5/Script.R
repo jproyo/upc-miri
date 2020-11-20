a23de241ce41abe53b88b530321e00480c78a915
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-11-20 09:49:59 +0100

    Adding material

--- csn/homeworks/homework_5/Script.R
+++ csn/homeworks/homework_5/Script.R
@@ -78,63 +78,59 @@ print_summary_model_results <- function(data, algos){
   print.data.frame(df, right=FALSE)
 }
 
+run_graph <- function(name, graph, algos){
+  print("")
+  print("####################################")
+  print(paste("          ", name, " GRAPH         "))
+  print("####################################")
+  print("")
+  
+  result <- NULL
+  for(algo in algos){
+    result <- rbind(result, execute_algorithm(algo, graph))
+  }
+  print_summary_model_results(result, algos)
+}
 
-main <- function(){
-  communities_algo_small <- c( "edge.betweenness.community"
-                             , "fastgreedy.community"
-                             , "label.propagation.community"
-                             , "leading.eigenvector.community"
-                             , "multilevel.community"
-                             , "optimal.community"
-                             , "spinglass.community"
-                             , "walktrap.community"
-                             , "infomap.community"
-                             )
 
-  communities_algo_big <- c( "edge.betweenness.community"
+communities_algo_small <- c( "edge.betweenness.community"
                            , "fastgreedy.community"
                            , "label.propagation.community"
                            , "leading.eigenvector.community"
                            , "multilevel.community"
+                           , "optimal.community"
                            , "spinglass.community"
                            , "walktrap.community"
                            , "infomap.community"
-  )
-  
+                          )
+
+communities_algo_big <- c( "edge.betweenness.community"
+                        , "fastgreedy.community"
+                        , "label.propagation.community"
+                        , "leading.eigenvector.community"
+                        , "multilevel.community"
+                        , "spinglass.community"
+                        , "walktrap.community"
+                        , "infomap.community"
+                        )
+
+main <- function(){
+ 
   graph_zachary <- graph.famous("Zachary")
   
   # Starred Repositories in Github https://snap.stanford.edu/data/github_stargazers.html
   graph_git <- graph.data.frame(read.csv('git_target.csv', header=TRUE), directed = FALSE)
   
-  # Arxiv HEP-TH (High Energy Physics - Theory)  https://snap.stanford.edu/data/ca-HepTh.html
-  graph_hep_th <- read.graph("ca-HepTh.txt", format = "edgelist", directed = FALSE)
+  # Arxiv GR-QC (General Relativity and Quantum Cosmology)  https://snap.stanford.edu/data/ca-GrQc.html
+  graph_gr_qc <- read.graph("ca-GrQc.txt", format = "edgelist", directed = FALSE)
   
-  # print("####################################")
-  # print("Zachary GRAPH")
-  # print("####################################")
-  # result_zachary <- NULL
-  # for(algo in communities_algo_small){
-  #   result_zachary <- rbind(result_zachary, execute_algorithm(algo, graph_zachary))
-  # }
-  # print_summary_model_results(result_zachary, communities_algo_small)
-  # 
-  # print("####################################")
-  # print("Github Starred Repos GRAPH")
-  # print("####################################")
-  # result_github <- NULL
-  # for(algo in communities_algo_big){
-  #   result_github <- rbind(result_github, execute_algorithm(algo, graph_git))
-  # }
-  # print_summary_model_results(result_github, communities_algo_big)
+  #run_graph("Zachary", graph_zachary, communities_algo_small)
   
-  print("####################################")
-  print("Hep-TH GRAPH")
-  print("####################################")
-  result_hep_th <- NULL
-  for(algo in communities_algo_big){
-    result_hep_th <- rbind(result_hep_th, execute_algorithm(algo, graph_hep_th))
-  }
-  print_summary_model_results(result_hep_th, communities_algo_big)
+  # Comment this if you want to run something quick
+  #run_graph("Github Starred Repos", graph_git, communities_algo_big)
+  
+  # Comment this if you want to run something quick
+  run_graph("GR-QC", graph_gr_qc, communities_algo_big)
   
 }
 

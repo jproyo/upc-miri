04af246c49fb63c15da17997c71efc4293295eaf
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-12-13 14:43:54 +0100

    Adding material

---
+++ csn/homeworks/final_work/Solution.R
@@ -0,0 +1,53 @@
+######################################################
+## Authors: Juan Pablo Royo Sales 
+## Title: Final Work
+## Dae: 2020-12-21
+####################################################
+
+library(igraph)
+library("data.table") 
+setwd("~/Projects/upc/upc-miri/csn/homeworks/final_work")
+
+dataFromGraph <- function(graph) {
+  table <- data.frame()
+  E = length(E(graph))
+  N = length(V(graph))
+  k = 2 * E / N
+  delta = 2 * E / (N * (N - 1))
+  table <- rbind(table, list(N, E, round(k, 2), round(delta, 6)))
+  colnames(table) = c("N", "E", "k", "delta")
+  return(table)
+}
+
+
+plotSome <- function(communities, graph, vecToShow) {
+  vertices_membership <- communities$membership
+  vertices_pos <- seq(length(communities$membership))
+  
+  for (sub_graph_index in vecToShow) {
+    vertices_sub_comm <- vertices_pos[vertices_membership == sub_graph_index]
+    sub_graph = induced_subgraph(graph, vids = vertices_sub_comm)
+    plot(sub_graph, main = paste("Community Number: ", sub_graph_index)
+         , layout = layout.auto,vertex.color = "blue"
+         , edge.color = "black")
+  }
+}
+
+main <- function(){
+  graph <- read.graph("graph.txt", format="edgelist", directed = FALSE)
+  
+  summary <- dataFromGraph(graph)
+  print.data.frame(summary, right=FALSE)
+  
+  communities <- walktrap.community(graph)
+  
+  count <- max(communities$membership)
+  print(paste("Number of Communities: ", count))
+  
+  plot(communities, main = "Communities"
+       , layout = layout.auto,vertex.color = "blue"
+       , edge.color = "black")
+  
+}
+
+main()

a6dec3442b8b2619662284ee294dfa4e5ebcbf6b
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-01-03 15:47:56 +0100

    Changing solution r script

--- csn/homeworks/final_work/Solution.R
+++ csn/homeworks/final_work/Solution.R
@@ -6,7 +6,7 @@
 
 library(sna)
 library(igraph)
-library("data.table") 
+library("data.table")
 setwd("~/Projects/upc/upc-miri/csn/homeworks/final_work")
 
 dataFromGraph <- function(graph) {
@@ -24,7 +24,7 @@ dataFromGraph <- function(graph) {
 plotSome <- function(communities, graph, vecToShow) {
   vertices_membership <- communities$membership
   vertices_pos <- seq(length(communities$membership))
-  
+
   for (sub_graph_index in vecToShow) {
     vertices_sub_comm <- vertices_pos[vertices_membership == sub_graph_index]
     sub_graph = induced_subgraph(graph, vids = vertices_sub_comm)
@@ -35,38 +35,38 @@ plotSome <- function(communities, graph, vecToShow) {
 }
 
 main <- function(){
-  fp_graphs <- c( "fp_graphs/small.dot", 
-                  "fp_graphs/cryptol.dot", 
-                  "fp_graphs/bad_design.dot", 
-                  "fp_graphs/pandoc.dot", 
+  fp_graphs <- c( "fp_graphs/small.dot",
+                  "fp_graphs/cryptol.dot",
+                  "fp_graphs/bad_design.dot",
+                  "fp_graphs/pandoc.dot",
                   "fp_graphs/cabal.dot")
-  oop_graphs <- c( "oop_graphs/bad_design.dot", 
+  oop_graphs <- c( "oop_graphs/bad_design.dot",
                    "oop_graphs/spring.dot",
                    "oop_graphs/joda_time.dot",
-                   "oop_graphs/guava.dot", 
+                   "oop_graphs/guava.dot",
                    "oop_graphs/akka.dot",
                    "oop_graphs/jetty.dot")
-  
+
   selected <- fp_graphs[1]
-  selected <- oop_graphs[2]
+  selected <- oop_graphs[5] # Another comment
   adj_matrix <- read.dot(selected)
   colnames(adj_matrix) <- c(1:length(adj_matrix[,1]))
   rownames(adj_matrix) <- c(1:length(adj_matrix[1,]))
   graph <- graph_from_adjacency_matrix(adjmatrix = adj_matrix, c("undirected"))
-  
+
   summary <- dataFromGraph(graph)
   print.data.frame(summary, right=FALSE)
-  
+
   communities <- walktrap.community(graph)
-  
+
   count <- max(communities$membership)
-  
+
   print(paste("Number of Communities: ", count))
-  
+
   plot(communities, graph, main = "Communities"
        , layout = layout.auto,vertex.color = "blue"
        , edge.color = "black")
-  
+
 }
 
 main()
